cad779cba020cd6148d69adaff186d997a8a06fe
Javier Gomez <javier.gov@outlook.es>
2024-01-14 22:47:07 -0600

    item carro

---
+++ src/main/java/com/xjgv/apiservlet/webapp/headers/models/ItemCarro.java
@@ -0,0 +1,28 @@
+package com.xjgv.apiservlet.webapp.headers.models;
+
+public class ItemCarro {
+
+    private int cantidad;
+    private Producto producto;
+
+    public ItemCarro(int cantidad, Producto producto) {
+        this.cantidad = cantidad;
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+}
